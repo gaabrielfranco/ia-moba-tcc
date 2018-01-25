7a5861db0cfbb76a5d050585ca7c861f60dc6a58
gaabrielfranco <gabriel.vita@ufv.br>
2018-01-24 23:51:00 -0200

    metricas

--- kdan.py
+++ kdan.py
@@ -8,6 +8,7 @@ Created on Mon Sep 25 14:32:34 2017
 #==============================================================================
 # PASSO 1: montar uma base equivalente ao KDAN, porem completa, com os atributos abaixo - Feito
 # PASSO 2: bolar uma metrica de eficiencia INDEPENDENTE de funcao (normalizar as variaveis da formula)
+#        (K - D + A + max(denies, LH) + max(hero_damage, hero_healing) + max(gpm, xp_p_min)) / Npartidas
 # PASSO 3: bolar uma metrica de eficiencia especifica para cada funcao (normalizar)
 # PASSO 4: comparar media, desvio padrao, coef. de variacao e maximo e minimo antes e depois da poda por outliers em cada metrica
 # PASSO 5: Fazer a mesma analise pra cada atributo individualmente (pra ver se surge algum insight)
@@ -27,42 +28,47 @@ Created on Mon Sep 25 14:32:34 2017
 # level => analogo a rpg (não vai ter utilidade)
 # xp_per_min
 # Os seguintes atributos estarao em arquivo separado
-# item_0 a item_5 (array de itens influencia na funcao do player) 
+# item_0 a item_5 (array de itens influencia na funcao do player)
 #==============================================================================
 
 import numpy as np
 from sklearn.cluster import KMeans
+
 
 def normaliza(x):
     xNorm = []
     minimo = np.min(x)
     maximo = np.max(x)
     for i in x:
-        xNorm.append((x-minimo)/(maximo-minimo))
+        xNorm.append((i - minimo) / (maximo - minimo))
 
     return xNorm
 
 # Parametro c arbitrario. Indica o quao relaxado se eh na poda
+
+
 def removeOutliers(dados, c=2.0):
     novo = []
     media = np.average(dados)
     desvio = np.std(dados)
     count = 0
     for d in dados:
-        if np.abs(media - d) < c*desvio:
+        if np.abs(media - d) < c * desvio:
             novo.append(d)
         else:
             count += 1
     return novo, count
 
+
 def sumario(eff):
     print('Media:', np.average(eff))
     print('Desvio:', np.std(eff))
-    print('Coef. Variacao:', np.std(eff)/np.average(eff))
+    print('Coef. Variacao:', np.std(eff) / np.average(eff))
     print('Minimo:', np.min(eff))
     print('Maximo:', np.max(eff))
 
-arq = '\\files\\KDAN.txt'
+
+arq = 'files/kdan.txt'
 
 fp = open(arq, 'rU')
 k = []
@@ -71,16 +77,25 @@ a = []
 n = []
 eff = []
 data = []
+
 for l in fp:
     partes = l.strip().split()
-    for i,p in enumerate(partes):
+    for i, p in enumerate(partes):
         partes[i] = int(p)
     k.append(partes[1])
     d.append(partes[2])
     a.append(partes[3])
     n.append(partes[4])
     data.append(partes[1:5])
-    eff.append((partes[1]+partes[3]-partes[2])/partes[4])
+    # data.append(partes[1:11])
+    eff.append((partes[1] + partes[3] - partes[2]) / partes[4])
+    # eff.append((partes[1] + partes[3] - partes[2] + max(partes[5], partes[9]) +
+    #            max(partes[7], partes[8]) + max(partes[6], partes[10])) / partes[4])
+    # (id_player,K, D, A, Npartidas, denies, gpm, hero_damage, hero_healing, LH, xp_p_min)
+    #(K - D + A + max(denies, LH) + max(hero_damage, hero_healing) + max(gpm, xp_p_min)) / Npartidas
+
+data = normaliza(data)
+eff = normaliza(eff)
 
 print("Antes da poda:")
 sumario(eff)
@@ -94,7 +109,7 @@ sumario(podado)
 km = KMeans(4)
 labels = km.fit_predict(data)
 
-contagens = [0]*4
+contagens = [0] * 4
 for l in labels:
     contagens[l] += 1
 
@@ -103,7 +118,7 @@ print(contagens)
 print("Antes da poda:")
 sumario(n)
 
-n2,nout = removeOutliers(n)
+n2, nout = removeOutliers(n)
 print('\nRemovidos %d de %d jogadores' % (nout, len(n)))
 print('\nDepois da poda')
 sumario(n2)
