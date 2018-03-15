d3996edd208726d0982e0ab1bef9ff2cad3034a0
Marcos Ribeiro <gabriel.vita@ufv.br>
2018-03-15 11:43:07 -0300

    Grafico de distribuicao

---
+++ scripts/plot_distribution.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 15 11:39:28 2018
+
+@author: marcos
+"""
+
+#https://seaborn.pydata.org/tutorial/distributions.html
+
+import numpy as np
+import seaborn as sns
+
+sns.set(color_codes=True)
+x = np.random.normal(size=100)
+sns.distplot(x)
