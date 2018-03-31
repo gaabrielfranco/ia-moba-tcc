7c78b80edd5840ed093e7a157ce4603c14f4882e
gaabrielfranco <gabriel.vita@ufv.br>
2018-03-31 19:20:13 -0300

    PCA fix + cluster eval

--- scripts/clusters_evaluation.py
+++ scripts/clusters_evaluation.py
@@ -3,12 +3,36 @@ import json
 
 
 def main():
-    data, data_corr, data_pruned = read_data('k-means_experiments')
+    kmeans, kmeans_corr, kmeans_pruned = read_data('k-means_experiments')
 
-    '''data_json = json.dumps(data, indent=4)
-    f = open('data.json', 'w')
-    f.writelines(data_json)
-    f.close()'''
+    f = open('files/output_cluster_evaluation/cluster_eval.txt', 'w')
+    for experiment in kmeans:
+        if experiment.split('_')[0] == 'all' or experiment.split('_')[0] == 'kda':
+            f.writelines("================ " +
+                         experiment + " ================\n")
+            for centroid in kmeans[experiment]['centroids']:
+                f.writelines(str(centroid) + '\n')
+            f.writelines('\n')
+    f.close()
+
+    f = open('files/output_cluster_evaluation/cluster_eval_corr.txt', 'w')
+    for experiment in kmeans_corr:
+        f.writelines("================ " +
+                     experiment + " ================\n")
+        for centroid in kmeans_corr[experiment]['centroids']:
+            f.writelines(str(centroid) + '\n')
+        f.writelines('\n')
+    f.close()
+
+    f = open('files/output_cluster_evaluation/cluster_eval_pruned.txt', 'w')
+    for experiment in kmeans_pruned:
+        if experiment.split('_')[0] == 'all' or experiment.split('_')[0] == 'kda':
+            f.writelines("================ " +
+                         experiment + " ================\n")
+            for centroid in kmeans_pruned[experiment]['centroids']:
+                f.writelines(str(centroid) + '\n')
+            f.writelines('\n')
+    f.close()
 
 
 if __name__ == "__main__":
