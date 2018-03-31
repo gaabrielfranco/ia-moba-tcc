67c9a9edd8e058a9568e191be2c5168463445dbd
gaabrielfranco <gabriel.vita@ufv.br>
2018-03-31 13:01:50 -0300

    Metric distribution plot

--- scripts/plot_metric.py
+++ scripts/plot_metric.py
@@ -17,9 +17,23 @@ def main():
     plot_distributions(data_dict, ['eff_metric'],
                        'files/output_plot_metric/', False, False)
 
-    kmeans, kmeans_corr, kmeans_pruned = read_data('k-means_experiments')
+    _, _, kmeans_pruned = read_data('k-means_experiments')
 
     plot_f(kmeans_pruned, 'pruned', 'files/output_plot_metric/', False)
+
+    for experiment in kmeans_pruned:
+        if experiment.split('_')[0] == 'all' or experiment.split('_')[0] == 'kda':
+            for k, cluster in enumerate(kmeans_pruned[experiment]['clusters']):
+                data = {}
+                data[experiment + '_C' + str(k + 1)] = [0] * \
+                    len(kmeans_pruned[experiment]['clusters'])
+                for player in cluster:
+                    data[experiment + '_C' + str(k + 1)].append(
+                        (player[0] + player[2]) / player[1])
+                plot_distributions(
+                    data, [experiment + '_C' + str(k + 1)], 'files/output_plot_metric/', False, True)
+                plot_distributions(
+                    data, [experiment + '_C' + str(k + 1)], 'files/output_plot_metric/', False, False)
 
 
 if __name__ == "__main__":
