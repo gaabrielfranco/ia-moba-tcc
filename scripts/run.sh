2977c1d6e27fa955a2b90fb043afb8c76ae36062
gaabrielfranco <gabriel.vita@ufv.br>
2018-05-19 16:22:04 -0300

    del sh

--- scripts/run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-python3 metrics_comparison_new.py -prj all
-python3 metrics_comparison_new.py -prj adg
-python3 metrics_comparison_new.py -prj kda
-python3 metrics_comparison_new.py -prj g
-python3 metrics_comparison_new.py -prj x
-
