406351d92589b2964a7e9db4c6409597d66857bb
gaabrielfranco <gabriel.vita@ufv.br>
2019-02-02 20:53:11 -0200

    Starting new database

---
+++ pipeline_v2/create_database_2/create_pro_database.py
@@ -0,0 +1,33 @@
+import time
+import json
+import urllib3
+
+
+def main():
+    http = urllib3.PoolManager()
+    f = open("matches.txt", "r")
+    matches = f.readlines()
+
+    count_not_human = 0
+    for match in matches:
+        new_match = match.replace("\n", "")
+        urllib3.disable_warnings()
+        r = http.request(
+            'GET', 'https://api.opendota.com/api/matches/' + new_match)
+
+        if r.status == 200:
+            print("Successful request! Match_id = ", new_match)
+            data_dict = json.loads(r.data.decode("utf-8"))
+            if data_dict["human_players"] == 10:
+                with open("pro_database/" + new_match + ".json", "w") as outfile:
+                    json.dump(data_dict, outfile)
+            else:
+                count_not_human += 1
+        else:
+            print("Request failed! Waiting 10 seconds...")
+            time.sleep(10)
+    print("Not human matches = %d" % count_not_human)
+
+
+if __name__ == "__main__":
+    main()
---
+++ pipeline_v2/create_database_2/request_matches_ids.py
@@ -0,0 +1,43 @@
+import urllib3
+import json
+import time
+
+
+def main():
+    # Initial request
+    http = urllib3.PoolManager()
+    urllib3.disable_warnings()
+    r = http.request('GET', 'https://api.opendota.com/api/proMatches')
+    data_dict = json.loads(r.data.decode("utf-8"))
+    matches_list = []
+    for match in data_dict:
+        matches_list.append(match["match_id"])
+
+    f = open("matches.txt", "w")
+    for match in matches_list:
+        f.write(str(match) + "\n")
+    f.close()
+
+    while True:
+        urllib3.disable_warnings()
+        r = http.request('GET', 'https://api.opendota.com/api/proMatches?less_than_match_id=' +
+                         str(matches_list[len(matches_list) - 1]))
+        data_dict = json.loads(r.data.decode("utf-8"))
+        if r.status == 200:
+            print("Successful request!")
+            matches_list = []
+            for match in data_dict:
+                matches_list.append(match["match_id"])
+
+            f = open("matches.txt", "a")
+            for match in matches_list:
+                f.write(str(match) + "\n")
+            f.close()
+
+        else:
+            print("Request failed! Waiting 10 seconds...")
+            time.sleep(10)
+
+
+if __name__ == "__main__":
+    main()
