4380df90210a5f2c7140d1bec97044e6eeb242f7
gaabrielfranco <gabriel.vita@ufv.br>
2019-02-03 16:59:10 -0200

    Colecting new database

--- pipeline_v2/create_database_2/create_pro_database.py
+++ pipeline_v2/create_database_2/create_pro_database.py
@@ -1,31 +1,34 @@
 import time
 import json
 import urllib3
+import glob
 
 
-def main():
+def main(): 
     http = urllib3.PoolManager()
     f = open("matches.txt", "r")
     matches = f.readlines()
-
     count_not_human = 0
+
     for match in matches:
         new_match = match.replace("\n", "")
-        urllib3.disable_warnings()
-        r = http.request(
-            'GET', 'https://api.opendota.com/api/matches/' + new_match)
+        while True:
+            urllib3.disable_warnings()
+            r = http.request(
+                'GET', 'https://api.opendota.com/api/matches/' + new_match)
 
-        if r.status == 200:
-            print("Successful request! Match_id = ", new_match)
-            data_dict = json.loads(r.data.decode("utf-8"))
-            if data_dict["human_players"] == 10:
-                with open("pro_database/" + new_match + ".json", "w") as outfile:
-                    json.dump(data_dict, outfile)
+            if r.status == 200:
+                print("Successful request! Match_id = ", new_match)
+                data_dict = json.loads(r.data.decode("utf-8"))
+                if data_dict["human_players"] == 10:
+                    with open("pro_database/" + new_match + ".json", "w") as outfile:
+                        json.dump(data_dict, outfile)
+                else:
+                    count_not_human += 1
+                break
             else:
-                count_not_human += 1
-        else:
-            print("Request failed! Waiting 10 seconds...")
-            time.sleep(10)
+                print("Request failed! Match_id = ", new_match, "Waiting 10 seconds for retry...")
+                time.sleep(10)
     print("Not human matches = %d" % count_not_human)
 
 
